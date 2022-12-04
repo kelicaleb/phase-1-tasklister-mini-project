7f33b39bec76eda4b4ea9bab87351dc8ec22d5ad
kelicaleb <kelicaleb7@gmail.com>
2022-12-04 18:39:55 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const createTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.querySelector("#new-task-description");
+  const tasks = document.querySelector("#tasks");
+
+  // creating new task
+  createTaskForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const newLi = document.createElement('li')
+    newLi.innerText = newTaskDescription.value
+    tasks.appendChild(newLi);
+    
+    const btn = document.createElement('button');
+    btn.textContent = 'Delete';
+    btn.style.fontSize = '15px'
+    tasks.lastElementChild.appendChild(btn);
+    // deleting a todo
+    btn.addEventListener('click', (e) => {
+      e.target.parentElement.remove();
+    })
+    // resitting the form fields
+    createTaskForm.reset();
+  }); 
 });
